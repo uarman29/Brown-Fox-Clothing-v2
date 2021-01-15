1c53e734b8945bea2f68d9d6d670fb59c99af777
Arman <uarman2215@gmail.com>
2021-01-15 16:26:36 -0500

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePageComponent from './components/pages/home-page/HomePageComponent';
+import CategorySelectionPageComponent from './components/pages/category-selection-page/CategorySelectionPageComponent';
+import CategoryPageComponent from './components/pages/category-page/CategoryPageComponent';
+import ItemPageComponent from './components/pages/item-page/ItemPageComponent';
+import CheckoutPageCheckout from './components/pages/checkout-page/CheckoutPageComponent';
 
 class App extends React.Component
 {
@@ -6,7 +13,15 @@ class App extends React.Component
     render()
     {
         return(
-            <div>App</div>
+            <div>
+                <Switch>
+                    <Route path="/:category/:subcategory/:itemId" component={ItemPageComponent}/>
+                    <Route path="/:category/:subcategory" component={CategoryPageComponent}/>
+                    <Route path="/categories" component={CategorySelectionPageComponent}/>
+                    <Route path="/checkout" component={CheckoutPageCheckout}/>
+                    <Route path="/" component={HomePageComponent}/>
+                </Switch>
+            </div>
         );
     }
 }
--- src/components/pages/category-page/CategoryPageComponent.js
+++ src/components/pages/category-page/CategoryPageComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CategoryPageComponent = () =>
 {
     return(
-        <div>CategoryP Page</div>
+        <div>Category Page</div>
     );
 }
 
