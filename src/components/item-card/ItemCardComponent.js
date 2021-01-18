e1f5d604710d33e2ea3fa0b784089c40992e14fa
Arman <uarman2215@gmail.com>
2021-01-18 13:18:23 -0500

    Started work on item page

--- src/components/item-card/ItemCardComponent.js
+++ src/components/item-card/ItemCardComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter} from 'react-router-dom';
 
 import './ItemCardComponent.css';
 
@@ -6,15 +7,22 @@ const ItemCardComponent = (props) =>{
     return(
         <div className="item-card">
             <div 
+                onClick={() => props.history.push(`${props.match.url}/${props.item.id}`)}
                 className="image"
                 style={{backgroundImage: `url(${props.item.imageUrl})`}}
             />
             <div className="item-footer">
-                <div className="name">{props.item.name}</div>
+                <div 
+                    onClick={() => props.history.push(`${props.match.url}/${props.item.id}`)} 
+                    className="name"
+                    style={{cursor:'pointer'}}
+                >
+                    {props.item.name}
+                </div>
                 <div className="price">${props.item.price}</div>
             </div>
         </div>
     );
 };
 
-export default ItemCardComponent;
+export default withRouter(ItemCardComponent);
--- src/components/pages/item-page/ItemPageComponent.js
+++ src/components/pages/item-page/ItemPageComponent.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
-const ItemPageComponent = () =>
+import SpinnerComponent from '../../spinner/SpinnerComponent';
+import './ItemPageComponent.css';
+
+const ItemPageComponent = (props) =>
 {
+    if(props.isLoading)
+        return <SpinnerComponent />;
+
+    const item = props.shopData[props.match.params.category]["categories"][props.match.params.subcategory]["items"][props.match.params.itemId];
     return(
-        <div>Item Page</div>
+        <div className="item-page">
+            <div className="item">
+                <div 
+                    className="item-image" 
+                    style={{backgroundImage: `url(${item.imageUrl})`}}
+                />
+                <div className="item-details">
+                    <div>{item.name}</div>
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default ItemPageComponent;
+const mapStateToProps = (state) =>
+{
+    return {isLoading: state.shopData.isLoading, shopData: state.shopData.data};
+}
+
+export default withRouter(connect(mapStateToProps)(ItemPageComponent));
