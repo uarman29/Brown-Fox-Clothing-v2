da67a76ca49102ba5032362a12885cb6d9a69bf0
Arman <uarman2215@gmail.com>
2021-01-24 13:29:21 -0500

    Added Styling and Regex to checkout page

--- src/components/pages/checkout-page/CheckoutPageComponent.js
+++ src/components/pages/checkout-page/CheckoutPageComponent.js
@@ -7,8 +7,13 @@ import './CheckoutPageComponent.css';
 
 const CheckoutPageComponent = (props) =>
 {
-    const [name, setName] = useState('');
-    const [shippingAddress, setShippingAddress] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zip, setZip] = useState('');
+    const [nameOnCard, setNameOnCard] = useState('');
     const [cardNumber, setCardNumber] = useState('');
     const [expirationDate, setExpirationDate] = useState('');
     const [CVV, setCVV] = useState('');
@@ -24,69 +29,133 @@ const CheckoutPageComponent = (props) =>
 
     return(
         <div className="checkout-page">
-            <div className="input-container">
-                <div className="user-info-container">
-                    <h2>User Details</h2>
-                    <form>
-                        <CustomFormInputComponent 
-                            label="Name" 
-                            type="text" 
-                            required 
-                            name="name" 
-                            value={name}
-                            onChange={(e) => setName(e.target.value)} 
-                        />
+            <form className="input-form" onSubmit={(e) => e.preventDefault()}>
+                <div className="input-container">
+                    <div className="user-info-container">
+                        <h2>User Details</h2>
+                        <hr />
+                        <div className="user-info-input">
+                            <CustomFormInputComponent 
+                                label="First Name" 
+                                type="text" 
+                                required 
+                                name="firstName" 
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
 
-                        <CustomFormInputComponent 
-                            label="Shipping Address" 
-                            type="text" 
-                            required 
-                            name="shippingAddress" 
-                            value={shippingAddress}
-                            onChange={(e) => setShippingAddress(e.target.value)} 
-                        />
+                            <CustomFormInputComponent 
+                                label="Last Name" 
+                                type="text" 
+                                required 
+                                name="lastName" 
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
+                    </div>
 
-                    </form>
+                    <div className="shipping-info-container">
+                        <h2>Shipping Details</h2>
+                        <hr />
+                        <div className="shipping-info-input">
+                            <CustomFormInputComponent 
+                                label="Address" 
+                                type="text" 
+                                required 
+                                name="address" 
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+
+                            <CustomFormInputComponent 
+                                label="City" 
+                                type="text" 
+                                required 
+                                name="city" 
+                                value={city}
+                                onChange={(e) => setCity(e.target.value)}
+                            />
+
+                            <CustomFormInputComponent 
+                                label="State" 
+                                type="text" 
+                                required 
+                                name="state" 
+                                value={state}
+                                onChange={(e) => setState(e.target.value)} 
+                            />
+
+                            <CustomFormInputComponent 
+                                label="Zip Code" 
+                                type="text" 
+                                required 
+                                name="zip" 
+                                value={zip}
+                                onChange={(e) => setZip(e.target.value)}
+                                pattern="^\d{5}$" 
+                            />
+
+                        </div>
+                    </div>
+
+
+                    <div className="card-info-container">
+                        <h2>Card Details</h2>
+                        <hr />
+                        <div className="card-info-input">
+                            <CustomFormInputComponent 
+                                label="Name on Card" 
+                                type="text" 
+                                required 
+                                name="nameOnCard" 
+                                value={nameOnCard}
+                                onChange={(e) => setNameOnCard(e.target.value)} 
+                            />
+
+                            <CustomFormInputComponent 
+                                label="Card Number" 
+                                type="text" 
+                                required 
+                                name="cardNumber" 
+                                value={cardNumber}
+                                onChange={(e) => setCardNumber(e.target.value)} 
+                                pattern="^\d{16}$"
+                            />
+
+                            <CustomFormInputComponent 
+                                label="Exp. Date" 
+                                type="text" 
+                                required 
+                                name="expirationDate" 
+                                value={expirationDate}
+                                onChange={(e) => setExpirationDate(e.target.value)} 
+                                placeholder="MM/YYYY"
+                                pattern="^(0[1-9]|10|11|12)\/20[2-9]{1}[1-9]{1}$"
+                            />
+
+                            <CustomFormInputComponent 
+                                label="CVV" 
+                                type="text" 
+                                required 
+                                name="CVV" 
+                                value={CVV}
+                                onChange={(e) => setCVV(e.target.value)}
+                                pattern="^[0-9]{3}$" 
+                            />
+
+                        </div>
+                    </div>
                 </div>
-                <div className="card-info-container">
-                    <h2>Card Details</h2>
-                    <form>
-                        <CustomFormInputComponent 
-                            label="Card Number" 
-                            type="text" 
-                            required 
-                            name="cardNumber" 
-                            value={cardNumber}
-                            onChange={(e) => setCardNumber(e.target.value)} 
-                        />
-
-                        <CustomFormInputComponent 
-                            label="Exp. Date" 
-                            type="text" 
-                            required 
-                            name="expirationDate" 
-                            value={expirationDate}
-                            onChange={(e) => setExpirationDate(e.target.value)} 
-                        />
-
-                        <CustomFormInputComponent 
-                            label="CVV" 
-                            type="text" 
-                            required 
-                            name="CVV" 
-                            value={CVV}
-                            onChange={(e) => setCVV(e.target.value)} 
-                        />
-
-                    </form>
+            
+                {getSubtotal()}
+                <div className="payment-details">
+                    SUBTOTAL: ${subTotal.toFixed(2)}<br/>
+                    SHIPPING: FREE <br />
+                    TOTAL: ${(subTotal).toFixed(2)}
+                    <CustomButtonComponent>Checkout</CustomButtonComponent>
                 </div>
-            </div>
-            {getSubtotal()}
-            <div className="payment-details">
-                SUBTOTAL: ${subTotal.toFixed(2)}<br/>
-                SHIPPING: FREE <br />
-                TOTAL: ${(subTotal).toFixed(2)}
-            </div>
+            </form>
         </div>
     )
 }
